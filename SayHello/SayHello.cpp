3e34b4e14bd21f7e0338c7e6932ceb61d7fc0bf7
CsbDontLikeCode <2698417371@qq.com>
2024-07-14 10:28:58 +0800

    "Config window in Prj-SayHello"
    Signed-off-by: CsbDontLikeCode <2698417371@qq.com>

--- SayHello/SayHello.cpp
+++ SayHello/SayHello.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main(void) {
-	std::cout << "Hello~" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "HelloWindow~", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Hello~" << std::endl;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -2,4 +2,5 @@
 
 int main() {
 	std::cout << "Waiting...Waiting..." << std::endl;
+	std::cout << "Here has a window now..." << std::endl;
 }
