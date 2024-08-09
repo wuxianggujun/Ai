6558915ceeb4fd586ccc02388ce89eb7f3954932
CsbDontLikeCode <2698417371@qq.com>
2024-08-10 00:09:04 +0800

    "引入离屏渲染"

--- AiCore/src/AiCore.cpp
+++ AiCore/src/AiCore.cpp
@@ -188,8 +188,36 @@ namespace Ai
 
 
 
+
     void renderAi()
     {
+        float quadVertices[] = {
+            -1.0f,  1.0f,  0.0f, 1.0f,
+            -1.0f, -1.0f,  0.0f, 0.0f,
+             1.0f, -1.0f,  1.0f, 0.0f,
+
+            -1.0f,  1.0f,  0.0f, 1.0f,
+             1.0f, -1.0f,  1.0f, 0.0f,
+             1.0f,  1.0f,  1.0f, 1.0f
+        };
+
+        // screen quad VAO
+        unsigned int quadVAO, quadVBO;
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+        Shader screenShader("resources/shaders/common/screen/screenQuad.vs", "resources/shaders/common/screen/screenQuad.fs");
+
+        screenShader.use();
+        screenShader.setInt("screenTexture", 0);
+
         for (int i = 0; i < 10; ++i)
         {
             // If point light in the contianer has been set, render them.
@@ -205,6 +233,30 @@ namespace Ai
             }
         }
 
+        // framebuffer configuration
+        // -------------------------
+        unsigned int framebuffer;
+        glGenFramebuffers(1, &framebuffer);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        // create a color attachment texture
+        unsigned int textureColorbuffer;
+        glGenTextures(1, &textureColorbuffer);
+        glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+
+        unsigned int rbo;
+        glGenRenderbuffers(1, &rbo);
+        glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); 
+
+        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+            std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
         while (!glfwWindowShouldClose(window))
         {
             float currentFrame = static_cast<float>(glfwGetTime());
@@ -217,6 +269,9 @@ namespace Ai
 
             // camera/view transformation
             glm::mat4 view = camera.GetViewMatrix();
+
+            // Bind the off-screen framebuffer.
+            glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
@@ -332,10 +387,23 @@ namespace Ai
                         break;
                     }
                 }
-                
-                glfwSwapBuffers(window);
-                glfwPollEvents();
             }
+
+            glBindFramebuffer(GL_FRAMEBUFFER, 0);
+            glDisable(GL_DEPTH_TEST);
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            screenShader.use();
+            glBindVertexArray(quadVAO);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+
+            
         }
         glDeleteVertexArrays(1, &g_triangle.VAO);
         glDeleteBuffers(1, &g_triangle.VBO);
---
+++ resources/shaders/common/screen/screenQuad.fs
@@ -0,0 +1,23 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D screenTexture;
+
+void main()
+{   
+    //// Normal
+    // vec3 col = texture(screenTexture, TexCoords).rgb;
+    // FragColor = vec4(col, 1.0);
+
+    //// Inversion
+    // FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0);
+
+    //// Grey
+    //FragColor = texture(screenTexture, TexCoords);
+    //float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+    //FragColor = vec4(average, average, average, 1.0);
+
+
+} 
